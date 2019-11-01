711e21b32e28191dcb7840f1d34ce57ffdee8ccf
Zykin S. Dmitry <zykin.dmitry@gmail.com>
2019-11-01 08:14:21 +0300

    Добавил папки для описания интерфейсов и классов 2D и 3D объектов соответственно и по файлу для описания интерфейса точки / вектора в каждом

---
+++ DevFactoryZ.SchemeExpert/Plane/IPoint.cs
@@ -0,0 +1,9 @@
+﻿namespace DevFactoryZ.SchemeExpert._2D
+{
+    /// <summary>
+    /// Этот интерфейс описывает точку / вектор на плоскости
+    /// </summary>
+    interface IPoint
+    {
+    }
+}
---
+++ DevFactoryZ.SchemeExpert/Volume/IPoint.cs
@@ -0,0 +1,9 @@
+﻿namespace DevFactoryZ.SchemeExpert.Volume
+{
+    /// <summary>
+    /// Этот интерфейс описывает точку / вектор в 3-х мерном пространстве
+    /// </summary>
+    interface IPoint
+    {
+    }
+}
