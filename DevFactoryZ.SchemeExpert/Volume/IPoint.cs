65e2ce0942900b7a4d9515b6f5e575676fcefed8
Andrey Maykut <maykout@yandex.ru>
2019-12-02 23:40:08 +0300

    Введен класс PointChangedEventArgs<IPoint>

--- DevFactoryZ.SchemeExpert/Volume/IPoint.cs
+++ DevFactoryZ.SchemeExpert/Volume/IPoint.cs
@@ -1,16 +1,21 @@
-﻿namespace DevFactoryZ.SchemeExpert.Volume
+﻿using System;
+using System.Collections.Generic;
+namespace DevFactoryZ.SchemeExpert.Volume
 {
     /// <summary>
     /// Этот интерфейс описывает точку / вектор в 3-х мерном пространстве
     /// </summary>
-    interface IPoint
+    public interface IPoint
     {
         double X { get; set; }
         double Y { get; set; }
-        void PutCoordinates(double X, double Y);
-        
-        //Событие, которое будет срабатывать при изменении координат точки
-        event System.EventHandler PointChanged;
 
+        event EventHandler<PointChangedEventArgs<IPoint>> OnLocationChanged;
+    }
+
+    public class PointChangedEventArgs<IPoint> : EventArgs
+    {
+        public IPoint OldLocation { get; set; }
+        public IPoint NewLocation { get; set; }
     }
 }
