639bb77c89886e8ee0d64fd771ec22f3c9109897
Andrey Maykut <maykout@yandex.ru>
2019-12-16 23:13:57 +0300

    Внесены исправления согласно замечаниям

--- DevFactoryZ.SchemeExpert/Plane/IPoint.cs
+++ DevFactoryZ.SchemeExpert/Plane/IPoint.cs
@@ -1,9 +1,25 @@
-﻿namespace DevFactoryZ.SchemeExpert._2D
+﻿using System;
+
+namespace DevFactoryZ.SchemeExpert._2D
 {
     /// <summary>
-    /// Этот интерфейс описывает точку / вектор на плоскости
+    /// Этот интерфейс описывает точку / вектор в 3-х мерном пространстве
     /// </summary>
-    interface IPoint
+
+    public interface IPoint : ICoordinate
     {
+        event EventHandler<PointChangedEventArgs> PointLocationChanged;
+    }
+
+    public class PointChangedEventArgs : EventArgs
+    {
+        public PointChangedEventArgs(ICoordinate previous, ICoordinate current)
+        {
+            Previous = previous;
+            Current = current;
+        }
+        
+        public ICoordinate Previous { get; }
+        public ICoordinate Current { get; }
     }
 }
